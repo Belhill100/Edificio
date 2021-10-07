a4a8e2e2ac40efbefd96bd4b9dc66fe40aafa715
Belhill100 <ecollazosbravo@gmail.com>
2021-10-06 19:35:20 -0500

    Primer Commit

---
+++ cmake-build-debug/CMakeFiles/Edificio.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Edificio.dir/main.cpp.obj"
+  "Edificio.exe"
+  "Edificio.exe.manifest"
+  "Edificio.pdb"
+  "libEdificio.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Edificio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
